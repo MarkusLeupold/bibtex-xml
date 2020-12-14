bba4db94bd8e539973ad4572806627856443c6b0
Markus Leupold <markus.leupold@web.de>
2020-12-14 20:58:40 +0100

    Remove unused imports

--- bibtoxml/src/Main.hs
+++ bibtoxml/src/Main.hs
@@ -1,11 +1,9 @@
 module Main where
 
-import Prelude
 import BibTeX.Parser
 import qualified BibTeX.Output.XML as BTXML (toElement)
 import qualified Text.XML.Light as XML (ppElement)
 import Data.String.Utils (strip)
-import Data.List (intercalate)
 
 main = do putStrLn "Please input the file path of the BibTeX file:"
           file   <- getLine >>= return . strip >>= readFile
