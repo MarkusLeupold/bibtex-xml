2550b63dc55fd2067715af83e8b7a10f47f20bef
Markus Leupold <markus.leupold@web.de>
2020-12-14 17:45:37 +0100

    Implement a simple test program for BibTeX entry XML output

--- bibtoxml/src/Main.hs
+++ bibtoxml/src/Main.hs
@@ -2,10 +2,14 @@ module Main where
 
 import Prelude
 import BibTeX.Parser
+import qualified BibTeX.Output.XML as BTXML (toElement)
+import qualified Text.XML.Light as XML (ppElement)
 import Data.String.Utils (strip)
+import Data.List (intercalate)
 
 main = do putStrLn "Please input the file path of the BibTeX file:"
           file   <- getLine >>= return . strip >>= readFile
           putStr "This is the parsed file:\n\n"
-          print $ parse file
+          (es, _) <- return $ parse file
+          putStr $ intercalate "\n" $ map (XML.ppElement . BTXML.toElement) es
           putStr "\n"
