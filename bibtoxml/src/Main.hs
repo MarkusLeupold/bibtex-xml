596f125a18e395488e958b267be7cb5a35f4cb3e
Markus Leupold <markus.leupold@web.de>
2020-12-14 21:57:29 +0100

    Implement main command line program

--- bibtoxml/src/Main.hs
+++ bibtoxml/src/Main.hs
@@ -1,13 +1,40 @@
 module Main where
 
-import BibTeX.Parser
+import BibTeX.Parser as BT
 import qualified BibTeX.Output.XML as BTXML (toElement)
 import qualified Text.XML.Light as XML (ppElement)
 import Data.String.Utils (strip)
 
-main = do putStrLn "Please input the file path of the BibTeX file:"
-          file   <- getLine >>= return . strip >>= readFile
-          putStr "This is the parsed file:\n\n"
-          es <- return $ parse file
-          putStr $ XML.ppElement $ BTXML.toElement es
-          putStr "\n"
+import System.Environment (getArgs, getProgName)
+import System.Exit (exitSuccess)
+
+
+main = getArgs >>= run >> exitSuccess
+
+xml_header :: String
+xml_header = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+
+convert :: String -> String
+convert s = xml_header
+            ++ "\n"
+            ++ ( XML.ppElement $ BTXML.toElement $ BT.parse s )
+            ++ "\n"
+
+processInput :: [String] -> IO String
+processInput [fpath] = do bibtex <- readFile fpath
+                          return $ convert bibtex
+processInput []      = do bibtex <- getContents
+                          return $ convert bibtex
+processInput _       = usage
+
+run :: [String] -> IO ()
+run ("-o":f:as) = do result <- processInput as
+                     writeFile f result
+run as          = do result <- processInput as
+                     putStrLn result
+
+usage = do progName <- getProgName
+           putStrLn $ "Usage: " ++ progName ++
+                      " [-o <output file>] [<input file>]"
+           exitSuccess
+
