59664cc1aa0e27e7f0a110f22c01d0264a61ec54
Markus Leupold <markus.leupold@web.de>
2021-01-01 20:16:07 +0100

    Total remake and extension of the tag value parsing system
    BibTeX.Parser.parseTagValue has been replaced by a call to the new higher-order
    BibTeX.Parser._parseTagValue, which takes a list of different value parsers as
    an argument. For each parser, _parseTagValue checks, if this parser can be
    applied to the input stream. The first matching parser is then used to read the
    tag value from the stream. This plug-in system makes it easier to see the
    different parsing mechanisms, and helps when changing them in the future. It
    also removes a lot of redundancy in the code.
    
    The value parser now supports string references and string concatenation.

--- bibtoxml/src/BibTeX/Parser.hs
+++ bibtoxml/src/BibTeX/Parser.hs
@@ -85,87 +85,83 @@ parseTagType s = let (t, r) = span Char.isAlpha s
                  _              -> (Just (UnknownTag t_lc), r)
 
 parseTagValue :: String -> (TagValue, String)
-parseTagValue s = case lstrip s of
+parseTagValue = _parseTagValue [ numberParser
+                               , referenceParser
+                               , delimitedValueParser
+                               ]
 
-    '"':cs -> let (v, r) = _parseTagValueQuoted cs in
-              case r of
-              '"':r' -> (LiteralValue v, r')
-              ""     -> error $ "parseTagValue{BibToXml}: found end of " ++
-                                "string when expecting the following " ++
-                                "character: '\"'"
-              r1:r'  -> error_midway
-                            r
-                            $ "parseTagValue{BibToXml}: found '"
-                              ++ [r1]
-                              ++ "' when expecting the following character: "
-                              ++ "'\"'"
+-- | a tuple describing a tag value parser
+-- 1st element: predicate function to determine if the parser can be applied to
+--              an input stream
+-- 2nd element: the actual parser function which returns the raw parsed value
+--              and the remaining input stream
+-- 3rd element: constructor function to turn the raw value into a TagValue
+type TagValueParser = ( String -> Bool
+                      , String -> (String, String)
+                      , String -> TagValue
+                      )
 
-    '{':cs -> let (v, r) = _parseTagValueBraced cs in
-              case r of
-              '}':r' -> (LiteralValue v, r')
-              ""     -> error $ "parseTagValue{BibToXml}: found end of "++
-                                "string when expecting the following " ++
-                                "character: '}'"
-              r1:r'  -> error_midway
-                            r
-                            $ "parseTagValue{BibToXml}: found '"
-                              ++ [r1]
-                              ++ "' when expecting the following character: '}'"
+numberParser :: TagValueParser
+numberParser = ( \ s -> (not (null s)) && (Char.isDigit (head s))
+               , span Char.isDigit
+               , LiteralValue
+               )
 
-    c:cs   -> if Char.isDigit c
-              then let (v, r) = span Char.isDigit cs in
-                   (LiteralValue (c:v), r)
-              else error_midway
-                       (c:cs)
-                       $ "parseTagValue{BibToXml}: found '"
-                         ++ [c]
-                         ++ "' when expecting a digit or one of the following "
-                         ++ "characters: '\"', '{'"
+isReferencedValueChar :: Char -> Bool
+isReferencedValueChar = Char.isAlpha
 
-    ""     -> error $ "parseTagValue{BibToXml}: found end of string when " ++
-                      "expecting a digit or one of the following " ++
-                      "characters: '\"', '{'"
+referenceParser :: TagValueParser
+referenceParser = ( \ s -> (not (null s)) && (isReferencedValueChar (head s))
+                  , span isReferencedValueChar
+                  , ReferencedValue
+                  )
+
+delimitedValueParser :: TagValueParser
+delimitedValueParser =
+    ( \ s -> (not (null s)) && (elem (head s) "\"{")
+    , \ (c:cs) -> let closingDelimiter = if c == '"' then '"' else '}'
+                      (v, r) = _parseDelimitedValue closingDelimiter 0 cs
+                  in if not (null r) && (head r) == closingDelimiter
+                         then (v, tail r)
+                         else error_midway
+                                  r
+                                  $ "delimitedValueParser{BibToXml}: could "
+                                    ++ "not find expected closing delimiter '"
+                                    ++ [closingDelimiter] ++ "'"
+    , LiteralValue
+    )
+
+-- | parse a tag value using one of the given parsers
+_parseTagValue :: [ TagValueParser ]
+               -> String
+               -> (TagValue, String)
+_parseTagValue ps cs =
+    let cs' = lstrip cs
+    in foldr ( \ (pr, f, cons) b ->
+                   if pr cs'
+                       then let (v_raw, r) = f cs'
+                                v = cons v_raw :: TagValue
+                            in case lstrip r of
+                                   '#':rs -> let (v', r') = _parseTagValue ps rs
+                                             in (ComposedValue v v', r')
+                                   _      -> (v, r)
+                            else b
+             )
+             ( error_midway cs $ "_parseTagValue{BibToXml}: Found no matching "
+                                 ++ "parser function."
+             )
+             ps
 
 
-_parseTagValueQuoted :: String -> (String, String)
-_parseTagValueQuoted ""          = ("", "")
--- A backslash (i.e. the escape character) lets the next character be
--- interpreted by TeX, so we aren't allowed to interpret it as a BibTeX control
--- character. Therefore, we copy it to the result without even looking at it.
-_parseTagValueQuoted ('\\':c:cs) = let (v, r) = _parseTagValueQuoted cs
-                                   in ('\\':c:v, r)
-_parseTagValueQuoted ('"':cs)    = ("", '"':cs)  -- end of value; put '"' back
-_parseTagValueQuoted (c:cs)      = let (v, r) = _parseTagValueQuoted cs
-                                   in (c:v, r)
-
-_parseTagValueBraced :: String -> (String, String)
-_parseTagValueBraced = __parseTagValueBraced 0
-
--- | Value parser for values inside curly braces. The first argument is the
--- | brace count inside the value. The surrounding delimiting braces of BibTeX
--- | aren't counted by this argument.
-__parseTagValueBraced :: Int -> String -> (String, String)
-__parseTagValueBraced _ ""          = ("", "")
-
--- Increment the brace count when encountering an opening curly brace (i.e. '{')
--- and continue parsing.
-__parseTagValueBraced b ('{':cs)    = let (v, r) = __parseTagValueBraced
-                                                   (b+1)
-                                                   cs
-                                      in ('{':v, r)
-
--- While the brace count is zero, unescaped closing curly braces (i.e. '}') are
--- not allowed to occur inside the value. This means, that if we encounter one
--- in this state we must have reached the end of the value. We put the brace
--- back into the input stream and return the empty string (i.e. the result
--- value) together with the remaining input stream.
-__parseTagValueBraced 0 ('}':cs)    = ("", '}':cs)  -- put '}' back
-
--- Decrement the brace count when encountering a closing curly brace (i.e. '}')
-__parseTagValueBraced b ('}':cs)    = let (v, r) = __parseTagValueBraced
-                                                   (b-1)
-                                                   cs
-                                      in ('}':v, r)
+-- | Value parser for literal values inside curly braces or quotes. The first
+-- argument is the character which terminates the value, the second argument is
+-- the current brace level inside the value. The surrounding delimiting braces
+-- of BibTeX aren't counted by this argument, so the initial call should be
+-- made with brace level zero.
+-- This function expects the input stream to begin with the first character of
+-- the value, i.e. the opening brace or quote must already have been removed.
+_parseDelimitedValue :: Char -> Int -> String -> (String, String)
+_parseDelimitedValue _ _ ""          = ("", "")
 
 -- A backslash (i.e. the TeX escape character) introduces a TeX control
 -- sequence. These are not touched by BibTeX, so we aren't allowed to interpret
@@ -173,15 +169,40 @@ __parseTagValueBraced b ('}':cs)    = let (v, r) = __parseTagValueBraced
 -- that the following character is a curly brace, TeX won't interpret it as a
 -- block beginning or end, because it is escaped.
 -- Therefore, we can copy the character after the backslash to the result
--- without even looking at it and we are sure that we don't need to change the
--- brace count.
-__parseTagValueBraced b ('\\':c:cs) = let (v, r) = __parseTagValueBraced b cs
-                                      in ('\\':c:v, r)
+-- without even looking at it and we are sure that we don't miss a brace level
+-- change.
+_parseDelimitedValue t b ('\\':c:cs) =
+    let (v, r) = _parseDelimitedValue t b cs
+    in ('\\':c:v, r)
+
+-- Increment the brace level when encountering an opening curly brace (i.e. '{')
+-- and continue parsing.
+_parseDelimitedValue t b ('{':cs) =
+    let (v, r) = _parseDelimitedValue t (b+1) cs
+    in ('{':v, r)
+
+-- While the brace level is zero, the terminating character means, that we have
+-- reached the end of the value. If we encounter it, we put it back into the
+-- input stream and return the empty string (i.e. the result value) together
+-- with the remaining input stream.
+-- Otherwise, put the character into the result and continue parsing. This will
+-- also eat closing curly braces which have no matching opening brace.
+_parseDelimitedValue t 0 (c:cs) =
+    if t == c
+        then ("", c:cs)
+        else let (v, r) = _parseDelimitedValue t 0 cs
+             in (c:v, r)
+
+-- Decrement the brace level when encountering a closing curly brace (i.e. '}')
+_parseDelimitedValue t b ('}':cs) =
+    let (v, r) = _parseDelimitedValue t (b-1) cs
+    in ('}':v, r)
 
 -- When none of the special cases above match the input, simply continue
 -- parsing.
-__parseTagValueBraced b (c:cs)      = let (v, r) = __parseTagValueBraced b cs
-                                      in (c:v, r)
+_parseDelimitedValue t b (c:cs) =
+    let (v, r) = _parseDelimitedValue t b cs
+    in (c:v, r)
 
 
 parseTag :: String -> (Maybe (TagType, TagValue), String)
@@ -288,4 +309,6 @@ parse s = let s' = strip_comments $ lstrip s in
 
 error_midway remaining message = error $ message
                                          ++ "\nAt:\n"
-                                         ++ (take 50 remaining)
+                                         ++ case take 50 remaining of
+                                                "" -> "\\0"
+                                                s  -> s
